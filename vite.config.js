32a8ec026cb16bf771d83f7bca1e594a7d33a770
lan4z <lan4zfps@gmail.com>
2023-11-14 09:00:01 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/coderlan4z/",
   server: {
     host: '0.0.0.0', 
     port: 3000, 
   },
 })
+
